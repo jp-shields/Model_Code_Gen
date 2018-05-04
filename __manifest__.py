2b46cdaabaa90b6601ab82222bdeb0eacc99b38b
jp-shields <shields.jp89@gmail.com>
2018-05-04 12:40:06 -0400

    Model Code Gen View Python Code to generate Model - This tool adds a menu item "Models" to your ODOO instance - From there you can see all the models in you instance (same as Settings / Technical / Database structure / Models) - Open a model to see the attributes including a new page tab "Code" - This tab contains a calculated field "repr" with attempts to generate the python code to generate the current model Note: This does not look at actual python files for base models: - as such can not read the many2many field correctly on base models - Also can not understand function, however it does list them as '# def method(self):' at the bottom

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Model_Code_Gen',
+    'version': '11.0.9.0',
+    'sequence': 5,
+    'summary': 'View Python Code to generate Model',
+    'category': 'Studio',
+    'author': 'Joe Shields',
+    'depends': [
+        'base','base_setup', "mail",
+    ],
+    'data': [
+        'views/ir_model_views.xml',
+        'menu/model_menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'OPL-1',
+    'images': [
+        'static/description/icon.png',
+    ]
+}
